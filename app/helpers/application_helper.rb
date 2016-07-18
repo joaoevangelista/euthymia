44fd1c4eddd1c931f6dc6c8b997bf3eab7193cd2
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-07-18 18:37:49 -0300

    Add tests for entry

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 # :nodoc:
 module ApplicationHelper
+  def update_time_of(entity)
+    return '' unless entity || entity.updated_at
+    entity.updated_at.strftime('%b %d, %Y - %H:%M')
+  end
 end
--- app/helpers/date_helper.rb
+++
@@ -1,6 +0,0 @@
-module DateHelper
-  def to_date(entity)
-    return '' unless entity || entity.updated_at
-    entity.updated_at.strftime('%b %d, %Y - %H:%M')
-  end
-end
