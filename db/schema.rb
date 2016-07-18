1cad6c52887b9288aa0ba564590dc9ca30595e3e
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-07-18 18:08:10 -0300

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160718165346) do
+ActiveRecord::Schema.define(version: 20160718202929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "entries", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "journal_id"
+    t.boolean  "favorite"
+    t.string   "title"
+    t.text     "body"
+    t.string   "accomplishment"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.index ["journal_id"], name: "index_entries_on_journal_id", using: :btree
+    t.index ["user_id"], name: "index_entries_on_user_id", using: :btree
+  end
 
   create_table "journals", force: :cascade do |t|
     t.string   "title"
@@ -41,5 +54,7 @@ ActiveRecord::Schema.define(version: 20160718165346) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "entries", "journals"
+  add_foreign_key "entries", "users"
   add_foreign_key "journals", "users"
 end
