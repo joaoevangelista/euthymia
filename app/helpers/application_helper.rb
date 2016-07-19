3c5b0c779d10c7e9533b18c05c21165afe0472f8
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-07-18 23:58:13 -0300

    Add loop for entries and a partial to render and easy maintain

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,18 @@ module ApplicationHelper
     return '' unless entity || entity.updated_at
     entity.updated_at.strftime('%b %d, %Y - %H:%M')
   end
+
+  def created_time_of(entity)
+    return '' unless entity || entity.created_at
+    entity.created_at.strftime('%b %d, %Y - %H:%M')
+  end
+
+  def short_text(text)
+    return '' unless text
+    if text.length > 140
+      text[0...140].concat('...')
+    else
+      text
+    end
+  end
 end
--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 # :nodoc:
 module EntriesHelper
+  def favorite(entry)
+    if entry.favorite
+      '<span class="star"><i class="fa fa-2x fa-star"></i></span>'.html_safe
+    else
+      '<span class="star"><i class="fa fa-2x fa-star-o"></i></span>'.html_safe
+    end
+
+  end
 end
