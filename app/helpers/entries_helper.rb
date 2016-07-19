0e8560d5df7fca01dde279cdc93108d9c802b02f
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-07-19 00:41:45 -0300

    Change favorite to be a heart and add a helper that will be replaced with a link with actions

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -3,10 +3,17 @@
 module EntriesHelper
   def favorite(entry)
     if entry.favorite
-      '<span class="star"><i class="fa fa-2x fa-star"></i></span>'.html_safe
+      '<span class="heart"><i class="fa fa-3x fa-heart"></i></span>'.html_safe
     else
-      '<span class="star"><i class="fa fa-2x fa-star-o"></i></span>'.html_safe
+      '<span class="heart"><i class="fa fa-3x fa-heart-o"></i></span>'.html_safe
     end
+  end
 
+  def favorite_button(entry)
+    if entry.favorite
+      '<span class="heart"><i class="fa fa-3x fa-heart"></i></span>'.html_safe
+    else
+      '<span class="heart"><i class="fa fa-3x fa-heart-o"></i></span>'.html_safe
+    end
   end
 end
