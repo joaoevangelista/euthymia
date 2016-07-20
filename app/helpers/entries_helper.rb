28cd9e5232c4314674fe9c177323e7d6eb0d2608
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-07-20 14:23:07 -0300

    Add entry favorite actions using heart button. Fix #4

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -11,9 +11,17 @@ module EntriesHelper
 
   def favorite_button(entry)
     if entry.favorite
-      '<span class="heart"><i class="fa fa-2x fa-heart"></i></span>'.html_safe
+      "<span class='heart'>
+      #{link_to icon('heart', '', class: 'fa-2x'),
+      favorite_journal_entry_path(entry.journal, entry),
+      method: :delete, class: 'heart'}
+      </span>".html_safe
     else
-      '<span class="heart"><i class="fa fa-2x fa-heart-o"></i></span>'.html_safe
+      "<span class='heart'>
+      #{link_to icon('heart-o', '', class: 'fa-2x'),
+      favorite_journal_entry_path(entry.journal, entry),
+      method: :put, class: 'heart'}
+      </span>".html_safe
     end
   end
 end
