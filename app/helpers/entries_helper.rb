13e30a8c09524509da7c74b015ba28c376172ef0
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-07-19 23:32:11 -0300

    Fix spacing

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -3,17 +3,17 @@
 module EntriesHelper
   def favorite(entry)
     if entry.favorite
-      '<span class="heart"><i class="fa fa-3x fa-heart"></i></span>'.html_safe
+      '<span class="heart"><i class="fa fa-2x fa-heart"></i></span>'.html_safe
     else
-      '<span class="heart"><i class="fa fa-3x fa-heart-o"></i></span>'.html_safe
+      '<span class="heart"><i class="fa fa-2x fa-heart-o"></i></span>'.html_safe
     end
   end
 
   def favorite_button(entry)
     if entry.favorite
-      '<span class="heart"><i class="fa fa-3x fa-heart"></i></span>'.html_safe
+      '<span class="heart"><i class="fa fa-2x fa-heart"></i></span>'.html_safe
     else
-      '<span class="heart"><i class="fa fa-3x fa-heart-o"></i></span>'.html_safe
+      '<span class="heart"><i class="fa fa-2x fa-heart-o"></i></span>'.html_safe
     end
   end
 
