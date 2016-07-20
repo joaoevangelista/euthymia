e39f59029d7fa9040539f2934295e6fbe6ce5e5e
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-07-20 18:03:58 -0300

    Fix scope of variable

--- app/jobs/indico_emotion_job.rb
+++ app/jobs/indico_emotion_job.rb
@@ -4,12 +4,12 @@ class IndicoEmotionJob < ApplicationJob
   queue_as :default
 
   def perform(entry)
-    emotions = Indico.emotion(@entry.body)
-    Emotion.new(entry: @entry, anger: emotions["anger"],
+    emotions = Indico.emotion(entry.body)
+    Emotion.new(entry: entry, anger: emotions["anger"],
                 joy: emotions["joy"],
                 fear: emotions["fear"],
                 sadness: emotions["sadness"],
                 surprise: emotions["surprise"],
-                user: @entry.user).save!
+                user: entry.user).save!
   end
 end
