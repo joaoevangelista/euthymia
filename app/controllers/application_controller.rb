b5ae14a94db2678d2bc3c549b14b8b2aa9bb610b
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-07-21 11:51:06 -0300

    Redirect user to journal path on sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@
 # :nodoc:
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # Override Devise method to redirect user to journals path
+  # when it succeeds the login
+  def after_sign_in_path_for(resource)
+    journals_path
+  end
+
 end
