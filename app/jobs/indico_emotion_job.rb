5622257cc130e43a44e05beec579bd582bfffc10
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-07-24 18:53:45 -0300

    Add update or create for indico jobs

--- app/jobs/indico_emotion_job.rb
+++ app/jobs/indico_emotion_job.rb
@@ -5,11 +5,20 @@ class IndicoEmotionJob < ApplicationJob
 
   def perform(entry)
     emotions = Indico.emotion(entry.body)
-    Emotion.new(entry: entry, anger: emotions['anger'],
-                joy: emotions['joy'],
-                fear: emotions['fear'],
-                sadness: emotions['sadness'],
-                surprise: emotions['surprise'],
-                user: entry.user).save!
+    emotion = Emotion.find_by entry: entry
+    emotions_hash = {
+      entry: entry,
+      anger: emotions['anger'],
+      joy: emotions['joy'],
+      fear: emotions['fear'],
+      sadness: emotions['sadness'],
+      surprise: emotions['surprise'],
+      user: entry.user
+    }
+    if emotion
+      emotion.update(emotions_hash)
+    else
+      Emotion.new(emotions_hash).save!
+    end
   end
 end
--- app/jobs/indico_sentiment_job.rb
+++ app/jobs/indico_sentiment_job.rb
@@ -6,6 +6,12 @@ class IndicoSentimentJob < ApplicationJob
   def perform(entry)
     return unless entry
     positivity = Indico.sentiment_hq(entry.body[0...1000])
-    Sentiment.new(entry: entry, user: entry.user, positivity: positivity).save
+    sentiment = Sentiment.find_by entry: entry
+    sentiment_hash = { entry: entry, user: entry.user, positivity: positivity }
+    if sentiment
+      sentiment.update(sentiment_hash)
+    else
+      Sentiment.new(sentiment_hash).save
+    end
   end
 end
