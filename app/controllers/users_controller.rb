f935290399f814b2f724af3e717fa3a56a6ed7b3
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-07-25 20:48:22 -0300

    Add password into continue registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email)
+    params.require(:user).permit(:email, :password)
   end
 
   def set_user
