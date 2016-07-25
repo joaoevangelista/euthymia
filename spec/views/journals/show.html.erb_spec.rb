9416757df5a063b4d5c25eec98cd77ae80026c26
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-07-25 20:37:52 -0300

    Fix offences

--- spec/views/journals/show.html.erb_spec.rb
+++ spec/views/journals/show.html.erb_spec.rb
@@ -3,6 +3,14 @@ require 'rails_helper'
 
 RSpec.describe 'journals/show', type: :view do
   before(:each) do
+  view.extend EmotionsHelper
+  @avg_emotions = {
+      joy: 0.3123,
+      anger: 0,
+      surprise: 0.2112,
+      sadness: 0.0021,
+      fear: 0.121
+    }
     @journal = FactoryGirl.create(:journal, user: FactoryGirl.create(:user))
   end
 
