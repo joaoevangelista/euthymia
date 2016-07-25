d44bc946b0272dd538e0677b7baeef930969f4f4
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-07-25 14:13:37 -0300

    Fix user crap code

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -7,7 +7,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success) if is_navigational_format?
+      set_flash_message(:notice, :success, kind: "#{auth.provider}".capitalize) if is_navigational_format?
     else
       session['devise.facebook_data'] = request.env['omniauth.auth']
       redirect_to new_user_registration_url
