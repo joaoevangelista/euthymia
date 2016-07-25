2b17d2d1eb98c92a957b0045c7e55500ff69b7fe
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-07-25 17:16:38 -0300

    Implement remember me on omniauth provider

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 # Override of Devise omniauth
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  include Devise::Controllers::Rememberable
+
   def provider
     auth = request.env['omniauth.auth']
     @user = User.find_for_oauth(auth, current_user)
 
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
+      remember_me(@user)
       set_flash_message(:notice, :success,
                         kind: auth.provider.to_s.capitalize) if is_navigational_format?
     else
