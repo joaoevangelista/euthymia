5f7bab9a7fbb3e53c354258d70e8fb4e391285cf
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-07-25 14:18:25 -0300

    Remove skip confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,7 +45,7 @@ class User < ApplicationRecord
           email: email ? email : "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
           password: Devise.friendly_token[0, 20]
         )
-        user.skip_confirmation!
+
         user.save!
       end
     end
