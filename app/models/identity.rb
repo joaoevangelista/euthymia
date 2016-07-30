209b112871995122ae45e0949453cbf09a41512e
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-07-30 12:06:02 -0300

    Add identity access_token param into schema

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -6,6 +6,11 @@ class Identity < ApplicationRecord
   validates_uniqueness_of :uid, scope: :provider
 
   def self.find_for_oauth(auth)
-    find_or_create_by(uid: auth.uid, provider: auth.provider)
+    identity = find_or_create_by(uid: auth.uid, provider: auth.provider)
+    if auth.credentials && auth.credentials.token
+      identity.access_token = auth.credentials.token
+      identity.save
+    end
+    identity
   end
 end
